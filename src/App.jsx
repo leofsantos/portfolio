556c1459c96f4e61d922b6840d3fb54f5a82057f
Leonardo dos Santos <leonardo.filipe.snts@gmail.com>
2024-04-21 20:59:07 -0700

    create Skills component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import styles from "./App.module.css";
 import { About } from "./components/About/About";
 import { Home } from "./components/Home/Home";
 import { Navbar } from "./components/Navbar/Navbar";
+import { Skills } from "./components/Skills/Skills";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Home />
       <About />
+      <Skills />
     </div>
   );
 }
