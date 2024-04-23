05da1daa3acb1a50ffa14aa662ed208ea6050ca9
Leonardo dos Santos <leonardo.filipe.snts@gmail.com>
2024-04-23 14:53:42 -0700

    create projects component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { About } from "./components/About/About";
 import { Home } from "./components/Home/Home";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Skills } from "./components/Skills/Skills";
+import { Projects } from "./components/Projects/Projects";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Home />
       <About />
       <Skills />
+      <Projects />
     </div>
   );
 }
---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./Projects.module.css";
+import { Cards } from "../Cards/Cards";
+
+export const Projects = () => {
+  return (
+    <section className={styles.container} id="projects">
+      <div className={styles.elipse}></div>
+      <div className={styles.content}>
+        <div className={styles.title}>
+          <h1>Projects</h1>
+        </div>
+        <div className={styles.cards}>
+          <Cards />
+        </div>
+      </div>
+    </section>
+  );
+};
