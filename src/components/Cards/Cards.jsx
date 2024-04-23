9994e7400b94ed73de0bd81d301d50711b229885
Leonardo dos Santos <leonardo.filipe.snts@gmail.com>
2024-04-23 14:52:31 -0700

    create cards component

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styles from "./Cards.module.css";
+import projects from "../../data/projects.json";
+import { getImageUrl } from "../../utils";
+
+export const Cards = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        {projects.map((skill, id) => {
+          return (
+            <div className={styles.card} key={id}>
+              <div className={styles.top}>
+                <img
+                  src={getImageUrl(skill.imageSrc)}
+                  alt={skill.imageDescription}
+                />
+              </div>
+              <div className={styles.text}>
+                <div>
+                  <h3>{skill.ProjectTitle}</h3>
+                </div>
+                <div>
+                  <p>{skill.ProjectDescription}</p>
+                </div>
+              </div>
+              <div className={styles.bottom}>
+                <button>
+                  <a href={skill.ProjectDemo}>VISIT HERE</a>
+                </button>
+                <a href={skill.ProjectRepo}>
+                  <i class="ri-github-fill"></i>
+                </a>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
